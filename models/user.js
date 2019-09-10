35b3d2299ff4d7ee3993bfb96ccc26e68a9a0b9f
SHAWN <shawnhan1999@hotmail.com>
2019-09-10 01:25:19 -0400

    Minor bug fix creating new user

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
     },
     status: {
         type: String,
-        default: 'I am new'
+        default: ''
     },
     posts: [{
         type: Schema.Types.ObjectId,
